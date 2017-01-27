da39433c1a67a85dc6a7b032e7cc0c6a609c7710
Jonathon Lantsberger <jtl378@mail.missouri.edu>
2017-01-26 22:53:46 -0600

    current revision

--- bits/src/bitmap.c
+++ bits/src/bitmap.c
@@ -1,4 +1,5 @@
 #include "../include/bitmap.h"
+#include <math.h>
 
 // data is an array of uint8_t and needs to be allocated in bitmap_create
 //      and used in the remaining bitmap functions. You will use data for any bit operations and bit logic
@@ -8,33 +9,84 @@
 
 bitmap_t *bitmap_create(size_t n_bits) {
 
-    return NULL;
+    if(n_bits <= 0) {
+        return NULL;
+    }
+
+    bitmap_t* map = malloc(n_bits);
+    map->bit_count = n_bits;
+    map->byte_count = ceil(n_bits/8.0);
+    map->data = calloc(map->byte_count, sizeof(*map->data));
+
+    return map;
 }
 
 bool bitmap_set(bitmap_t *const bitmap, const size_t bit) {
 
-	return false;
+    if(bitmap == NULL || bitmap->bit_count <= bit) {
+        return false;
+    }
+
+    if(bit < 0) {
+        return false;
+    }
+
+    bitmap->data[bit/8] |= (uint8_t)1 << (bit & 7);
+
+    return true;
 }
 
 bool bitmap_reset(bitmap_t *const bitmap, const size_t bit) {
 
-	return false;
+    if(bitmap == NULL || bitmap->bit_count <= bit) {
+        return false;
+    }
+
+    if(bit < 0) {
+        return false;
+    }
+
+    bitmap->data[bit/8] &= ~((uint8_t)1 << (bit & (7)));
+
+    return true;
 }
 
 bool bitmap_test(const bitmap_t *const bitmap, const size_t bit) {
-	return false;
+
+    if(bitmap == NULL) {
+        return false;
+    }
+
+    if(bitmap->bit_count <= bit) {
+        return false;
+    }
+
+    if((bitmap->data[bit/8] >> (bit & 7)) == 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 size_t bitmap_ffs(const bitmap_t *const bitmap) {
 
-	return 0;
+    return 0;
 }
 
 size_t bitmap_ffz(const bitmap_t *const bitmap) {
 
-	return 0;
+    return 0;
 }
 
 bool bitmap_destroy(bitmap_t *bitmap) {
-	return false;
+
+    if(bitmap == NULL) {
+        return false;
+    }
+
+    free(bitmap->data);
+    free(bitmap);
+
+    return true;
 }
--- system_programming/src/sys_prog.c
+++ system_programming/src/sys_prog.c
@@ -13,21 +13,110 @@
 
 bool bulk_read(const char *input_filename, void *dst, const size_t offset, const size_t dst_size) {
 
-	return false;
+    if(input_filename == NULL || dst == NULL) { //checking for NULL inputs
+        return false;
+    }
+
+    if(offset < 0 || dst_size <= 0) { //checking for negative values
+        return false;
+    }
+
+    int file = open(input_filename, O_RDONLY); //opening the input file for reading
+
+    if(file <= 0) { //if the file was unable to be opened a number <= 0 is set as the file descriptor
+        return false;
+    }
+
+    if(lseek(file, offset, SEEK_CUR) != offset) { //seeking in the file for the offset provided
+        close(file); 
+        return false;
+    }
+
+    if((read(file, dst, dst_size)) <= 0) { //reading the data into the dst array
+        close(file);
+        return false;
+    }
+    else{
+        close(file); //cleaning up upon success
+        return true;
+    }
 }
 
 bool bulk_write(const void *src, const char *output_filename, const size_t offset, const size_t src_size) {
+    if(src == NULL || output_filename == NULL) { // checking for NULL inputs
+        return false;
+    }
 
-	return false;
+    if(offset < 0 || src_size <= 0) { //checking for negative inputs
+        return false;
+    }
+
+    int file = open(output_filename, O_WRONLY); //opening the file for writing
+
+    if(file <= 0) { //error check to ensure the file was opened correctly
+        return false;
+    }
+
+    if(lseek(file, offset, SEEK_CUR) != offset) { //seeking as we did in the read function
+        close(file);
+        return false;
+    }
+
+    if((write(file, src, src_size)) <= 0) { //writing out to the file from the source array and checking the number of bytes it wrote
+        close(file);
+        return false;
+    }
+    else {
+        close(file); //cleaning up
+        return true;
+    }
 }
 
 
 bool file_stat(const char *query_filename, struct stat *metadata) {
 
-	return false;
+    if(query_filename == NULL || metadata == NULL) { //checking for NULL inputs
+        return false;
+    }
+
+    int file = open(query_filename, O_RDONLY); //opening the file for reading
+
+    if(file <= 0) { //checking for a good file open
+        return false;
+    }
+
+    if(fstat(file, metadata) < 0) { //using fstat to pull the metadata from the input file, a result < 0 indicates that the reading of the data failed
+        close(file);
+        return false;
+    }
+    else {
+        close(file); //cleaning up
+        return true;
+    }
 }
 
 bool endianess_converter(uint32_t *src_data, uint32_t *dst_data, const size_t src_count) {
 
-	return false;
+    if(src_data == NULL || dst_data == NULL) { //error checks for NULL inputs
+        return false;
+    }
+
+    if(src_count <= 0) { //ensuring the source has content
+        return false;
+    }
+
+    int i; 
+    uint32_t input, byte1, byte2, byte3, byte4; //declaring the variables we need for the swap
+    for(i = 0; i < src_count; i++) {
+        input = src_data[i]; //pulling the first data item from the input
+
+        byte1 = ((input>>24)&0x000000ff); //shifting the furthest left bit all the way to the right and then anding it with hex to ensure its a 32 bit int
+        byte2 = ((input<<8)&0xff000000); //moving the furthest left byte over 2 places to put it as the 2nd from the left byte
+        byte3 = ((input>>8)&0xff000000); //moving the furthest left byte into the 2 place from the right
+        byte4 = ((input<<24)&0xff000000);//moving the furthest left byte to the furthest right bit
+
+        dst_data[i] = byte1 | byte2 | byte3 | byte4; //the ORing of these bytes will create the full uint32 that has been swapped
+    }
+
+    return true;
 }
