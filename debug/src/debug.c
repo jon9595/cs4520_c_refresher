9d792d254c5361b4de66e416805d086f02b9d28c
Jonathon Lantsberger <jtl378@mail.missouri.edu>
2017-01-31 13:56:41 -0600

    finished debug function

--- debug/src/debug.c
+++ debug/src/debug.c
@@ -6,25 +6,35 @@
 
 // protected function, that only this .c can use
 int comparator_func(const void *a, const void *b) {
-    return (uint8_t *)a - (uint8_t *)b;
+    return *(uint8_t *)a - *(uint8_t *)b;
 }
 
 bool terrible_sort(uint16_t *data_array, const size_t value_count) {
-    uint16_t sorting_array *= malloc(value_count * sizeof(*data_array));
-    for (int i = 0; i < value_count; ++i) {
+    if(data_array == NULL) {
+        return false;
+    }
+
+    if(value_count == 0) {
+        return false;
+    }
+
+    uint16_t* sorting_array = malloc(value_count * sizeof(uint16_t));
+
+    for (unsigned int i = 0; i < value_count; ++i) {
         sorting_array[i] = data_array[i];
     }
 
-    qsort(sorting_array, value_count, sizeof(sorting_array) / sizeof(uint16_t), comparator_func)
+    qsort(sorting_array, value_count, sizeof(uint16_t), comparator_func);
 
     bool sorted = true;
-    for (int i = 0; i < value_count ++i) {
-        sorted &= sorting_array[i] < sorting_array[i + 1];
+    for (unsigned int i = 1; i < value_count; ++i) {
+        sorted &= sorting_array[i - 1] <= sorting_array[i];
     }
 
     if (sorted) {
-        memcpy(sorting_array, *data_array, value_count);
+        memcpy(data_array, sorting_array, value_count * sizeof(uint16_t));
     }
+    free(sorting_array);
     return sorted;
 }
 
