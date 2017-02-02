d8619ac531e8a2ca8d45fb25c4852f44baa2b871
Jonathon Lantsberger <jtl378@mail.missouri.edu>
2017-02-02 15:36:26 -0600

    Finished all modules

--- error_handling/src/error_handling.c
+++ error_handling/src/error_handling.c
@@ -10,30 +10,79 @@
 
 int create_blank_records(Record_t **records, const size_t num_records) {
 
-	*records = (Record_t*) malloc(sizeof(Record_t) * num_records);
+    if(*records != NULL || num_records == 0 || records == NULL) { //checking for a non-null *records pointer, a 0 number of records and a NULL records pointer
+        return -1;
+    }
 
-	memset(*records,0,sizeof(Record_t) * num_records);
-	return 0;	
+    if(num_records > SIZE_MAX) { //checking for a bad number of num_records. size_t is unsigned so setting it to < 0 will result in a huge size of num_records
+        return -2;
+    }
+
+    *records = (Record_t*) malloc(sizeof(Record_t) * num_records);
+
+    if(*records == NULL) { //checking to ensure the pointer was malloced correctly
+        return -2;
+    }
+
+    memset(*records,0,sizeof(Record_t) * num_records); //setting all values in the records array to 0
+
+    return 0; //mission accomplished
 }
 
 int read_records(const char *input_filename, Record_t *records, const size_t num_records) {
 
-	int fd = open(input_filename, O_RDONLY);
+    if(input_filename == NULL || records == NULL) { //checking for NULL inputs
+        return -1;
+    }
 
-  ssize_t data_read = 0;
-  for (size_t i = 0; i < num_records; ++i) {
-		data_read = read(fd,&records[i], sizeof(Record_t));	
-	}
-	return 0;
+    if(num_records == 0) { //checking for a 0 record input
+        return -1;
+    }
+
+    int fd = open(input_filename, O_RDONLY); //opening the file for reading
+
+    if(fd < 0) { //file failed to open
+        return -2;
+    }
+
+    ssize_t data_read = 0; //setting the initial size of the bytes read
+    for (size_t i = 0; i < num_records; ++i) {
+        data_read = read(fd,&records[i], sizeof(Record_t)); //reading in the data into each record in the array
+
+        if(data_read != sizeof(Record_t)) { //if the number of bytes read does not equal the size of a record, there was a read error
+            return -3;
+        }
+    }
+    return 0;
 }
 
 int create_record(Record_t **new_record, const char* name, int age) {
 
-	*new_record = (Record_t*) malloc(sizeof(Record_t));
-	
-	memcpy((*new_record)->name,name,sizeof(char) * strlen(name));
-	(*new_record)->name[MAX_NAME_LEN - 1] = 0;	
-	(*new_record)->age = age;
-	return 0;
+    if(*new_record != NULL || name == NULL) { //checking to ensure the new_record is NULL and the name parameter is not null
+        return -1;
+    }
 
+    if(age < 1 || age > 200) { //checking for an out of bound age
+        return -1;
+    }
+
+    if(strcmp(name, "\n") == 0) { //the bad input name that is passed contains a newline char so we need to check for this
+        return -1;
+    }
+
+    if(strlen(name) > 50) { //checking that the length of the input string is of the right length
+        return -1;
+    }
+
+    *new_record = (Record_t*) malloc(sizeof(Record_t)); //creating space for a new record
+
+    if(*new_record == NULL) { //checking for bad memory allocation
+        return -2;
+    }
+
+    memcpy((*new_record)->name,name,sizeof(char) * strlen(name)); //copying in the new data
+    (*new_record)->name[MAX_NAME_LEN - 1] = 0;	//ending the string correctly
+    (*new_record)->age = age; //setting the age
+
+    return 0;
 }
--- structures/src/structures.c
+++ structures/src/structures.c
@@ -4,37 +4,104 @@
 
 int compare_structs(sample_t* a, sample_t* b)
 {
-	return 0;
+    if(a == NULL || b == NULL) { //checking for bad inputs
+        return 0;
+    }
+
+    if(a->a != b->a) { //ensuring that a's member a = b's member a
+        return 0;
+    }
+
+    if(a->b != b->b) {
+        return 0;
+    }
+
+    if(a->c != b->c) {
+        return 0;
+    }
+
+    return 1; //the structs matched
 }
 
 void print_alignments()
 {
-	printf("Alignment of int is %zu bytes\n",__alignof__(int));
-	printf("Alignment of double is %zu bytes\n",__alignof__(double));
-	printf("Alignment of float is %zu bytes\n",__alignof__(float));
-	printf("Alignment of char is %zu bytes\n",__alignof__(char));
-	printf("Alignment of long long is %zu bytes\n",__alignof__(long long));
-	printf("Alignment of short is %zu bytes\n",__alignof__(short));
-	printf("Alignment of structs are %zu bytes\n",__alignof__(fruit_t));
+    printf("Alignment of int is %zu bytes\n",__alignof__(int));
+    printf("Alignment of double is %zu bytes\n",__alignof__(double));
+    printf("Alignment of float is %zu bytes\n",__alignof__(float));
+    printf("Alignment of char is %zu bytes\n",__alignof__(char));
+    printf("Alignment of long long is %zu bytes\n",__alignof__(long long));
+    printf("Alignment of short is %zu bytes\n",__alignof__(short));
+    printf("Alignment of structs are %zu bytes\n",__alignof__(fruit_t));
 }
 
 int sort_fruit(const fruit_t* a,int* apples,int* oranges, const size_t size)
 {
-	return 0;
+    if(a == NULL || apples == NULL || oranges == NULL) { //checking for NULL inputs
+        return -1;
+    }
+    if(size <= 0) { //checking for a bad size input
+        return -1;
+    }
+
+    for(unsigned int i = 0; i < size; i++) {
+        if(a[i].type == 1) { //finding a 1 means this is an apple
+            *(apples) += 1;
+        }
+        else { //this means the element is an orange
+            *(oranges) += 1;
+        }
+    }
+
+    return size; //size was already passed, and it is the size of the array so pass this back
 }
 
 int initialize_array(fruit_t* a, int apples, int oranges)
 {
-	return 0;
+    if(a == NULL) { //checking for bad input array
+        return -1;
+    }
+
+    if(apples < 0 || oranges < 0) { //checking for negative inputs
+        return -1;
+    }
+
+    int i = 0, j = 0;
+
+    for(i = 0; i < apples; i++) {
+        initialize_apple((apple_t*)a + i); //casting a as an apple then using pointer arithmitic to increment the pointer to the next item
+    }
+
+    for(j = i; j < i + oranges; j++) { //starting this loop where the last loop left off
+        initialize_orange((orange_t*)a + i); //doing the same pointer casting as we did above
+    }
+
+    return 0;
 }
 
 int initialize_orange(orange_t* a)
 {
-	return 0;
+    if(a == NULL) { //checking for NULL inputs
+        return -1;
+    }
 
+    //setting all orange attributes to 0 (type = 0 indicated oranges)
+    a->peeled = 0;
+    a->type = 0;
+    a->weight = 0;
+
+    return 0; //initialization worked
 }
 
 int initialize_apple(apple_t* a)
 {
-	return 0;
+    if(a == NULL) { //checking for NULL inputs
+        return -1;
+    }
+
+    //setting all attributes to 0, and type to 1, which represents an apple
+    a->type = 1;
+    a->weight = 0;
+    a->worms = 0;
+
+    return 0;
 }
